e786fc195e418694217204d7ad55440af00420a2
jcosmao <josselin.cosmao@gmail.com>
2020-11-25 08:33:05 +0100

    deploy version

--- botCheckers/tiktok_bot_checker/TikTokChecker.js
+++ botCheckers/tiktok_bot_checker/TikTokChecker.js
@@ -1,3 +1,5 @@
+const tiktok_scraper = require('tiktok-scraper')
+
 class TikTokChecker{
     constructor(data){
         this.user = data.user
@@ -7,7 +9,8 @@ class TikTokChecker{
         let score = this.getFollowersScore() + this.getHeartScore() + this.getVerifiedScore() + this.getProfileScore() + this.getVideosScore()
         console.log(`Final score: ${score}`)
         return score;
-    }
+	}
+	// calculates followers score
     getFollowersScore(){
         const followers = this.user.fans;
         const following = this.user.following;
@@ -22,7 +25,8 @@ class TikTokChecker{
         console.log('      getFollowersScore : ' + finalScore.toFixed(2));
 
         return finalScore;
-    }
+	}
+	// calculates heart(like) score
     getHeartScore() {
         const hearts = this.user.heart
         let finalScore = 0;
@@ -33,21 +37,24 @@ class TikTokChecker{
         console.log('      getHeartScore : ' + finalScore.toFixed(2));
 
         return finalScore;
-    }
+	}
+	// checks whether user is verified
     getVerifiedScore() {
         let finalScore = 0
         if (this.user.verified) finalScore = 500;
         else finalScore = 0
         console.log('      getVerifiedScore : ' + finalScore.toFixed(2));
         return finalScore
-    }
+	}
+	// calculates profile score
     getProfileScore() {
         let finalScore = 0
         if (this.user.signature) finalScore += 50
         if (this.user.openFavoruite) finalScore += 50
         console.log('      getProfileScore : ' + finalScore.toFixed(2));
         return finalScore
-    }
+	}
+	// calculates videos score
     getVideosScore(){
         let finalScore = 0
         const {avrgPlays, avrgShares, avrgComms} = this.videos
@@ -61,4 +68,115 @@ class TikTokChecker{
         return finalScore
     }
 }
-module.exports = TikTokChecker
+// gets user followers
+async function getTikTokFollowers(username){
+	const user = await getTikTokUserData(username)
+	if (user){
+		if (!user.isSecret)
+			return user.fans
+		else 
+			return false
+	}else{
+		return false
+	}
+}
+// gets general result
+async function getTikTokResult(username){
+	const num  = await getTikTokNumerical(username)
+	let checks = {
+		isBot: null,
+		error: null,
+		errorCode: 200
+	}
+	switch(num){
+		case 0:
+			checks.isBot = false
+		break
+		case 1:
+			checks.isBot = true
+		break
+		case 400:
+			checks.error = 'API Error'
+			checks.errorCode = 400
+		break
+		case 500:
+			checks.error = 'TikTok API Error'
+			checks.errorCode = 500
+		break
+	}
+	return checks
+}
+// gets numerical result
+async function getTikTokNumerical(username) {
+    const data = await getTikTokFullData(username);
+	if (data){
+		try{
+			if (data.user.isSecret){
+				return 400
+			}else{
+				const tikChecker = new TikTokChecker(data)
+				const score = tikChecker.getFinalScore()
+				if (score > 200){
+					return 0 // not bot
+				}else {
+					return 1
+				}
+			}
+		}catch(e){
+			return 400
+		}
+	}else{
+		return 500
+	}
+}
+// gets full profile data
+async function getTikTokFullData(username){
+	const user = await getTikTokUserData(username)
+	const videos = await getTikTokUserVideos(username)
+	if (user && videos) {
+		return {
+			user,
+			videos
+		}
+	}else{
+		return false
+	}
+}
+// gets user data
+async function getTikTokUserData(username) {
+	try {
+        const data = await tiktok_scraper.getUserProfileInfo(username)
+		return data
+	}catch(e) {
+        console.log(e)
+		return false
+	}
+}
+// gets users video
+async function getTikTokUserVideos(userId) {
+	 try {
+		 const data = await tiktok_scraper.user(userId, {
+			 number: 50
+		 })
+		 if (data.collector.length){
+			 const videos = data.collector;
+			let avrgPlays = 0,
+				avrgShares = 0,
+				avrgComms = 0;
+			 videos.forEach(val => {
+				avrgPlays+=val.playCount
+				avrgShares+=val.shareCount
+				avrgComms+=val.commentCount
+			 })
+			 avrgPlays = parseInt(avrgPlays / videos.length)
+			 avrgShares = parseInt(avrgShares / videos.length)
+			 avrgComms = parseInt(avrgComms / videos.length)
+			 return {videos: true, avrgPlays, avrgShares, avrgComms}
+		 }else {
+			 return {videos: false}
+		 }
+	 }catch(e) {
+		 return false
+	 }
+}
+module.exports = {getTikTokFollowers, getTikTokResult}
--- botCheckers/twitch_bot_checker/TwitchChecker.js
+++ botCheckers/twitch_bot_checker/TwitchChecker.js
@@ -1,3 +1,15 @@
+const {
+    promisify
+} = require('util')
+const config = require('config')
+const requestAsyncTwitch = promisify(require('request').defaults({
+    headers: {
+		'Accept': 'application/vnd.twitchtv.v5+json',
+        'Client-ID': config.get('twitch.client_id')
+    },
+    rejectUnauthorized: false
+}))
+
 class TwitchChecker {
     constructor(data) {
         this.data = data
@@ -84,4 +96,103 @@ class TwitchChecker {
         return views / videos.length
     }
 }
-module.exports = TwitchChecker
+// gets general result
+async function getTwitchResult(login){
+	const num  = await getTwitchNumericResult(login)
+	let checks = {
+		isBot: null,
+		error: null,
+		errorCode: 200
+	}
+	switch(num){
+		case 0:
+			checks.isBot = false
+		break
+		case 1:
+			checks.isBot = true
+		break
+		case 400:
+			checks.error = 'API Error'
+			checks.errorCode = 400
+			
+		break
+		case 500:
+			checks.error = 'Twitch API Error'
+			checks.errorCode = 500
+		break
+	}
+	return checks
+}
+// generates numerical result
+async function getTwitchNumericResult(login) {
+    let checks = {
+        isBot: null,
+        errors: []
+    }
+    // Retrieves twitch user data from API 
+    const userData = await getTwitchUserData(login)
+    // If there's an error in retrieving returns error with it's text
+    if (userData.error) {
+        console.log(`Error: ${userData}`)
+        return 500
+    }
+    console.log('Successfully retrieved user data')
+    // Create an instance of TwitchChecker class with provided data
+    const twitchChecker = new TwitchChecker(userData)
+    try {
+        // Get  score
+        const score = twitchChecker.getCompleteScore()
+        if (score > 100) return 0
+		else return 1
+    } catch (e) {
+        // If there is an error return it
+        console.log(e)
+		return 400
+    }
+    return 1
+}
+async function getTwitchUserData(login) {
+    console.log(`Getting data for ${login}`)
+
+    let userData = await requestAsyncTwitch(`https://api.twitch.tv/kraken/users?login=${login}`)
+	// if first request is successfull then all other will be
+	// console.log(userData.body)
+    if (userData.statusCode === 200) {
+        userData = JSON.parse(userData.body).users
+        if (userData.length) {
+            userData = userData[0]
+			let userFollows = (await requestAsyncTwitch(`https://api.twitch.tv/kraken/users/${userData._id}/follows/channels`)).body
+			let  userFollowers = (await requestAsyncTwitch(`https://api.twitch.tv/kraken/channels/${userData._id}`)).body
+            let userVideos = (await requestAsyncTwitch(`https://api.twitch.tv/kraken/channels/${userData._id}/videos`)).body
+            userFollowers = JSON.parse(userFollowers)
+            userFollows = JSON.parse(userFollows)
+            userVideos = JSON.parse(userVideos)
+
+            // Check whether user has videos
+			if (userVideos.status === 404 || !userVideos._total) userVideos = false
+            return {
+                error: false,
+                data: userData,
+                followers: userFollowers.followers,
+                follows: userFollows._total,
+                videos: userVideos.videos
+            }
+        } else return {
+            error: true,
+			errorCode: 500
+        }
+    } else return {
+        error: true,
+        errorCode: 500
+    }
+}
+// gets users' followers
+async function getTwitchFollowers(login){
+	const user = await getTwitchUserData(login)
+	if (!user.error)
+		return user.followers;
+	else 
+		return false
+}
+
+module.exports = { getTwitchResult, getTwitchFollowers }
